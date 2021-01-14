49253b632c16c6b638079601312c161812999a15
Juan Buitrago <juanbuitrago2002@outlook.com>
2021-01-14 17:25:03 -0500

    agregando estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,25 +1,30 @@
 import React from "react";
 
+import "./styles/Badge.css";
 import ConfLogo from "../images/badge-header.svg";
 
 class badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={ConfLogo} alt="Logo de la imgen"></img>
         </div>
-        <div>
-          <img src="https://www.gravatar.com/avatar/HASH" alt="Avatar"></img>
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar  "
+            src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+            alt="Avatar"
+          ></img>
           <h1>
             JUAN <br /> BUITRAGO
           </h1>
         </div>
-        <div>
-          <p>descripcion</p>
-          <p>@junBU</p>
+        <div className="Badge__section-info">
+          <h3>Cojonudo</h3>
+          <div>@junBu</div>
         </div>
-        <div>
+        <div className="Badge__footer">
           <a>#Sandiego</a>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./global.css";
 
 import Badge from "./components/Badge";
 const container = document.getElementById("app");
