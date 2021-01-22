5b8c2f982c44a098c01df46a40237fa30655fbd9
Juan Buitrago <juanbuitrago2002@outlook.com>
2021-01-22 13:07:30 -0500

    terminando Badge list

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 
 import BadgeNew from "./pages/BadgeNew";
+import Badge from "./pages/Badge.js";
 const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__,__dónde__)
-ReactDOM.render(<BadgeNew />, container);
+ReactDOM.render(<Badge />, container);
